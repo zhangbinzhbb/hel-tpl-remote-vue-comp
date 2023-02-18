b976032c686855caf65cee2e56c66343b1b970a3
zhangbinzhbb <13020106399@163.com>
2023-02-18 15:50:17 +0800

    add: 测试模块

--- subApp.js
+++ subApp.js
@@ -8,6 +8,18 @@ const pkg = require('./package.json');
 // const subApp = helDevUtils.createVue2SubApp(pkg, { npmCdnType: 'unpkg', homePage: 'http://my-unpkg:8888' });
 
 // deploy to git
-const subApp = helDevUtils.createVue2SubApp(pkg, { homePage: 'https://github.com/zhangbinzhbb/hel-tpl-remote-vue-comp/as_v1/v1.0.0' });
+const subApp = helDevUtils.createVue2SubApp(pkg, { homePage: 'https://github.com/zhangbinzhbb/hel-tpl-remote-vue-comp/as_v1' });
+
+// console.log("subApp===>",subApp)
+// [ 
+//     homePage: 'https://github.com/zhangbinzhbb/hel-tpl-remote-vue-comp/as_v1/v1.0.0',
+//     groupName: 'hel-tpl-remote-vue-comps',
+//     name: 'hel-tpl-remote-vue-comps',
+//     externals: { vue: 'LEAH_Vue' },
+//     getExternals: [Function: getExternals],
+//     jsonpFnName: 'helJsonp_hel-tpl-remote-vue-comps_1676704164942',
+//     getPublicPathOrUrl: [Function: getPublicPathOrUrl],
+//     distDir: 'hel_dist' 
+// ]
 
 module.exports = subApp;
